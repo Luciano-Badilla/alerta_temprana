8b9ea323fecfcf94bea5517fa3aa474aee4fdad0
Luciano Badilla <lucianobadilla88@gmail.com>
2024-10-25 08:23:43 -0300

    Tarea programada añadida, errores en login corregidos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMiF8EyHQnSh2QVu9LfxH1Ftw0x8jCGRd7EQUb" crossorigin="anonymous">
+        crossorigin="anonymous">
 </head>
 
 <body>
